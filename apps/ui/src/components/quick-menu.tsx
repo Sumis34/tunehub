3891a90a95a66f682053734d9480e87ae15625ba
Noe Krebs <eon.sberk@gmail.com>
2026-01-07 19:35:32 +0100

    feat: refactor quick menu code

--- apps/ui/src/components/quick-menu.tsx
+++ apps/ui/src/components/quick-menu.tsx
@@ -1,85 +1,54 @@
-import {
-  animate,
-  useMotionValue,
-  motion,
-  type PanInfo,
-} from "framer-motion";
+import { animate, useMotionValue, motion, type PanInfo } from "framer-motion";
 
 export default function QuickMenu({ children }: { children: React.ReactNode }) {
-  const DRAG_AREA_HEIGHT = 44;
-  const TOP_BUFFER_HEIGHT = 200;
+  const OPEN_HEIGHT = 0.2;
+  const HANDLE_HEIGHT = 44;
+  const SNAP_THRESHOLD = 0.1;
 
-  const SCREEN_HEIGHT = window.innerHeight;
-  const THRESHOLD = SCREEN_HEIGHT * 0.1;
-  const TOP_OFF_SCREEN = -SCREEN_HEIGHT - TOP_BUFFER_HEIGHT + DRAG_AREA_HEIGHT;
-  const BOTTOM = -TOP_BUFFER_HEIGHT;
-  const BOTTOM_DRAG_CONSTRAINT = BOTTOM + 20;
+  const h = window.innerHeight;
+  const closed = -h;
+  const open = -h * OPEN_HEIGHT;
+  const paddingTop = h * OPEN_HEIGHT;
 
-  const y = useMotionValue(TOP_OFF_SCREEN);
+  const y = useMotionValue(closed);
 
   const handleDragEnd = (
     _event: MouseEvent | TouchEvent | PointerEvent,
     info: PanInfo
   ) => {
-    const currentY = y.get();
-    const dragDirection = info.offset.y;
-    const isDirectionUp = dragDirection < 0;
+    const threshold = h * SNAP_THRESHOLD;
+    const isDraggingUp = info.offset.y < 0;
 
-    if (isDirectionUp && currentY > BOTTOM - THRESHOLD) {
-      animate(y, BOTTOM, {
-        type: "spring",
-        stiffness: 600,
-        damping: 50,
-      });
-      return;
+    let target = closed;
+
+    if (isDraggingUp && y.get() > open - threshold) {
+      target = open;
+    } else if (!isDraggingUp && y.get() < closed + threshold) {
+      target = closed;
+    } else if (!isDraggingUp) {
+      target = open;
     }
 
-    if (!isDirectionUp && currentY < TOP_OFF_SCREEN + THRESHOLD) {
-      console.log("here");
-
-      animate(y, TOP_OFF_SCREEN, {
-        type: "spring",
-        stiffness: 600,
-        damping: 50,
-      });
-      return;
-    }
-
-    if (isDirectionUp) {
-      animate(y, TOP_OFF_SCREEN, {
-        type: "spring",
-        stiffness: 600,
-        damping: 50,
-      });
-      return;
-    }
-
-    animate(y, BOTTOM, {
-      type: "spring",
-      stiffness: 600,
-      damping: 50,
-    });
+    animate(y, target, { type: "spring", stiffness: 600, damping: 50 });
   };
 
   return (
-    <div>
+    <div className="relative">
       <motion.div
         drag="y"
-        style={{
-          paddingBottom: DRAG_AREA_HEIGHT,
-          height: SCREEN_HEIGHT + TOP_BUFFER_HEIGHT,
-          y: y,
-        }}
-        dragConstraints={{
-          top: TOP_OFF_SCREEN,
-          bottom: BOTTOM_DRAG_CONSTRAINT,
-        }}
+        style={{ paddingBottom: HANDLE_HEIGHT, height: h + HANDLE_HEIGHT, y }}
+        dragConstraints={{ top: closed, bottom: open + 20 }}
         dragElastic={0}
         onDragEnd={handleDragEnd}
-        className="w-full absolute pb-32 inset-0 pointer"
+        className="absolute inset-0 pointer"
       >
-        <div className="w-screen bg-neutral-900 h-full flex flex-col items-center rounded-b-xl">
-          <div className="flex-1"></div>
+        <div
+          style={{
+            paddingTop,
+          }}
+          className="bg-neutral-900 h-full flex flex-col items-center rounded-b-xl"
+        >
+          <div className="flex-1">asd</div>
           <div className="p-4">
             <div className="w-24 h-2 bg-neutral-800 rounded-full" />
           </div>
